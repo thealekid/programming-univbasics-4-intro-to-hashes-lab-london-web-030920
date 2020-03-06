53d9525257e91d9c05ae10006ef89f4f96ee38f5
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 17:37:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,15 +18,7 @@ def my_hash_creator(key, value)
   my_hash_creator = {
     key => value
   }
-end
-
-def read_from_hash(hash, key)
-  hash[key]
-end
-
-def update_counting_hash(hash, key)
-  if hash[key]
-    hash [key] += 1 
+end[key] += 1 
   else
     hash[key] = 1 
 end
